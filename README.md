1d62e17e47e227fb260c90c3cbb786fc4ba235fe
Mark B <virtimus@gmail.com>
2025-06-25 17:52:10 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# evkey
-key-hooks for linuch 
+## evKey - a low lewel keybard hooks for linuch
+
+Modified evtest.c to accomodate some usefull key-hook/shortcuts functions ...
+
+## Compile
+
+gcc -o evkey evkey.c
+
+## Run
+
+./startup.sh
---
+++ startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo mkdir -p /dev/input/evkey
+sudo chown $USER /dev/input/evkey
+#sudo mkdir -p /dev/evkey-shortcuts
+homeDir=~
+sudo mkdir -p /dev/input/evkey-shortcuts
+if [ -e $homeDir/evkey-shortcuts ]; then 
+   sudo cp $homeDir/evkey-shortcuts/* /dev/input/evkey-shortcuts/
+sudo chown $USER /dev/input/evkey-shortcuts
+
+fi
+#cd /src/evkey && sudo nice -n19 ./evkey simple cpulimit -l 50 --
+#cd /src/evkey && sudo nice -n19 ./evkey simple
+sudo ln -sf /dev/input/by-path/*-kbd /dev/input/kbd
+sudo chown $USER /dev/input/kbd
+
+cd /src/evkey
+./evkey /dev/input/kbd &
+
+#read -p "Press enter to continue"
+
+
+
